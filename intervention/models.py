d003a7d1762bfd0378744cf6f95a6bcd2d170468
unknown <velimovichKL@Gmail.com>
2023-02-02 21:56:50 +0300

    work on work_case model

--- intervention/models.py
+++ intervention/models.py
@@ -3,10 +3,11 @@ from django.db import models
 
 class WorkCase(models.Model):
     case_id = models.CharField(max_length=255)  # to be reviewed
-    customer_name = models.CharField(max_length=255)  # to be reviewed
-    customer_address = models.CharField(max_length=255)  # to be reviewed
-    machine_type = models.CharField(max_length=255)  # to be reviewed
-    machine_serial_number = models.CharField(max_length=255)  # to be reviewed
-    machine_installation_year = models.CharField(max_length=255)  # to be reviewed
+    customer_name = models.CharField(max_length=255)  # move to customer model
+    customer_address = models.CharField(max_length=255)  # move to customer model
+    machine_id = models.CharField(max_length=255)
+    # machine_type = models.CharField(max_length=255)  # move to machine model
+    # machine_serial_number = models.CharField(max_length=255)  # move to machine model
+    # machine_installation_year = models.CharField(max_length=255)  # move to machine model
     problem_description = models.CharField(max_length=255)  # to be reviewed
     technician_assigned_id = models.CharField(max_length=255)  # to be reviewed
