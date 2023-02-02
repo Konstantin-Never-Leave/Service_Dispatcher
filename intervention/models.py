2ec292e5a9c5cfcdba26db5e3d7935280882868b
unknown <velimovichKL@Gmail.com>
2023-02-02 21:45:16 +0300

    add_model_workase

--- intervention/models.py
+++ intervention/models.py
@@ -1,4 +1,12 @@
 from django.db import models
 
+
 class WorkCase(models.Model):
-    pass
+    case_id = models.CharField(max_length=255)  # to be reviewed
+    customer_name = models.CharField(max_length=255)  # to be reviewed
+    customer_address = models.CharField(max_length=255)  # to be reviewed
+    machine_type = models.CharField(max_length=255)  # to be reviewed
+    machine_serial_number = models.CharField(max_length=255)  # to be reviewed
+    machine_installation_year = models.CharField(max_length=255)  # to be reviewed
+    problem_description = models.CharField(max_length=255)  # to be reviewed
+    technician_assigned_id = models.CharField(max_length=255)  # to be reviewed
